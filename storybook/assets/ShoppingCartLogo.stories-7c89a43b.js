bf47643e6142f4346690136fc3a3592fff2f00fd
sh981013s <sh981013s@users.noreply.github.com>
2023-05-11 08:18:16 +0000

    deploy: 19ca49af4689fd2265c187cb15686cdf7a0fb2fd

--- storybook/assets/ShoppingCartLogo.stories-7c89a43b.js
+++
@@ -1,9 +0,0 @@
-import{S as i}from"./ShoppingCartLogo-02d215e0.js";import"./index-c013ead5.js";import"./_commonjsHelpers-725317a4.js";const s={title:"ShoppingCartLogo",component:i,argTypes:{fill:{control:{type:"color"}},isFlipped:{control:{type:"boolean"}},width:{control:{type:"number"}},height:{control:{type:"number"}}}},o={args:{width:200,height:200,isFlipped:!1,fill:"#000"}};var r,e,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    width: 200,
-    height: 200,
-    isFlipped: false,
-    fill: '#000'
-  }
-}`,...(t=(e=o.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const l=["Primary"];export{o as Primary,l as __namedExportsOrder,s as default};
-//# sourceMappingURL=ShoppingCartLogo.stories-7c89a43b.js.map
